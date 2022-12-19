4dd2b842b12c6ead5c8944f073f95225abb280bf
kiran_a <iamkiranashok@gmail.com>
2022-12-19 21:54:48 +0530

    init commit

--- src/AddThousandNumbers.java
+++ src/AddThousandNumbers.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class AddThousandNumbers {
+public class AddThousandNumbers {
+    public static void main(String[] args)
+    {
+        final int LOWERBOUND=1;
+        final int UPPERBOUND=1000;
+        int number=LOWERBOUND;
+        int sum=0;
+        while(number<=UPPERBOUND)
+        {
+            sum = sum+number;
+            ++number;
+        }
+        System.out.println("the sum from" + LOWERBOUND + "to" + UPPERBOUND +"is" + sum);
+
+    }
 }
